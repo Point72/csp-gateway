d59c938f0a22d8361d9b1e886c3514349d7a3e68
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2026-02-03 23:22:10 -0500

    Revamp std lib logging to match logfire config
    Signed-off-by: Tim Paine <3105306+timkpaine@users.noreply.github.com>

--- csp_gateway/server/modules/logging/__init__.py
+++ csp_gateway/server/modules/logging/__init__.py
@@ -7,5 +7,11 @@ from .logfire import (
 )
 from .opsgenie import PublishOpsGenie
 from .printing import PrintChannels
-from .stdlib import LogChannels
+from .stdlib import (
+    LogChannels,
+    Logging,
+    StdlibLogging,  # Backwards compatibility alias
+    configure_stdlib_logging,
+    is_stdlib_logging_configured,
+)
 from .symphony import PublishSymphony
